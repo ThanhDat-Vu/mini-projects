b9ea11073bd0a6d254dc6ce5404336058ee6538d
Daniel <20521181@gm.uit.edu.vn>
2022-08-11 15:22:53 +0700

    Pure React

---
+++ Englix-and-chill/src/index.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return React.createElement(
+    'h1',
+    {},
+    'Hello World'
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(App));
