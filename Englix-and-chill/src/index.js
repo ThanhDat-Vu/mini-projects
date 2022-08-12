47310777390b982b7aab058a6a2ad5a72b4e7f99
Daniel <20521181@gm.uit.edu.vn>
2022-08-12 16:56:36 +0700

    Prettier & Git

--- Englix-and-chill/src/index.js
+++ Englix-and-chill/src/index.js
@@ -3,11 +3,11 @@ const Transcript = (props) => {
     React.createElement("span", {}, props.subcategory),
     React.createElement("span", {}, props.index),
     React.createElement("h3", {}, props.title),
-  ])
-}
+  ]);
+};
 
 const App = () => {
-  return React.createElement('h1', {}, [
+  return React.createElement("h1", {}, [
     React.createElement(Transcript, {
       subcategory: "Season 1",
       index: "01",
@@ -23,8 +23,8 @@ const App = () => {
       index: "01",
       title: "Return of the Shirt",
     }),
-  ])
-}
+  ]);
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(React.createElement(App));
