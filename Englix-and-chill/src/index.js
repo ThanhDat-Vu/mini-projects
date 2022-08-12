fc3245093ff1f760691ddd34796ed5bd394fefa9
Daniel <20521181@gm.uit.edu.vn>
2022-08-12 15:18:48 +0700

    Components

--- Englix-and-chill/src/index.js
+++ Englix-and-chill/src/index.js
@@ -1,9 +1,29 @@
+const Transcript = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("span", {}, props.subcategory),
+    React.createElement("span", {}, props.index),
+    React.createElement("h3", {}, props.title),
+  ])
+}
+
 const App = () => {
-  return React.createElement(
-    'h1',
-    {},
-    'Hello World'
-  )
+  return React.createElement('h1', {}, [
+    React.createElement(Transcript, {
+      subcategory: "Season 1",
+      index: "01",
+      title: "Pilot",
+    }),
+    React.createElement(Transcript, {
+      subcategory: "Season 1",
+      index: "01",
+      title: "Sweet Taste of Liberty",
+    }),
+    React.createElement(Transcript, {
+      subcategory: "Season 1",
+      index: "01",
+      title: "Return of the Shirt",
+    }),
+  ])
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
