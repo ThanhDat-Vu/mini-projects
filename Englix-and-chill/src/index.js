e02615c5b4a938388b687c6347cf900d8cf2e335
Daniel <20521181@gm.uit.edu.vn>
2022-08-13 21:43:15 +0700

    ESLint

--- Englix-and-chill/src/index.js
+++ Englix-and-chill/src/index.js
@@ -1,30 +1,30 @@
-const Transcript = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("span", {}, props.subcategory),
-    React.createElement("span", {}, props.index),
-    React.createElement("h3", {}, props.title),
+function Transcript(props) {
+  return React.createElement('div', {}, [
+    React.createElement('span', {}, props.subcategory),
+    React.createElement('span', {}, props.index),
+    React.createElement('h3', {}, props.title),
   ]);
-};
+}
 
-const App = () => {
-  return React.createElement("h1", {}, [
+function App() {
+  return React.createElement('h1', {}, [
     React.createElement(Transcript, {
-      subcategory: "Season 1",
-      index: "01",
-      title: "Pilot",
+      subcategory: 'Season 1',
+      index: '01',
+      title: 'Pilot',
     }),
     React.createElement(Transcript, {
-      subcategory: "Season 1",
-      index: "01",
-      title: "Sweet Taste of Liberty",
+      subcategory: 'Season 1',
+      index: '01',
+      title: 'Sweet Taste of Liberty',
     }),
     React.createElement(Transcript, {
-      subcategory: "Season 1",
-      index: "01",
-      title: "Return of the Shirt",
+      subcategory: 'Season 1',
+      index: '01',
+      title: 'Return of the Shirt',
     }),
   ]);
-};
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(React.createElement(App));
